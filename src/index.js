be2267ae1f1deab7a8c95a460d4ac87dfc286e3b
balaji vasi <balajivasi@gmail.com>
2017-10-10 20:06:29 -0400

    created redux
    created redux folders

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {render} from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css'
+import './css/main.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render("Balaji", document.getElementById('sri'));
 registerServiceWorker();
